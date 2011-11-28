ae27ba8aa2bf16c8bacc6adac3e32a820b726232
Andrew Dinn <adinn@redhat.com>
2011-11-28 09:33:54 +0000

    split junit tests into 2 stages to show first tracing and then fault injection

--- junit/src/test/java/org/my/BytemanJUnitTests.java
+++ junit/src/test/java/org/my/BytemanJUnitTests.java
@@ -32,7 +32,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
- * Test class showing how to use Byteman BMUnit package with JUnit
+ * Test class showing how to use Byteman BMUnit package with JUnit to do simple tracing
  */
 @RunWith(BMUnitRunner.class)
 @BMScript(value="trace", dir="target/test-classes")
@@ -62,94 +62,4 @@ public class BytemanJUnitTests
         assert(output.equals("hello mum! goodbye cruel mum, goodbye!\n"));
     }
 
-    /**
-     * this is the same code as the previous test but it uses a Byteman rule to throw an IOException in
-     * the pattern replacer before it can processes any text lines. The pattern replacer thread should catch
-     * the exception, close its input and output streams and exit, causing the writer to finish
-     * with an empty output. All the pipeline threads shoudl exit cleanly.
-     * @throws Exception
-     */
-    @Test
-    @BMRule(name="throw IOException at 1st transform",
-            targetClass = "TextLineProcessor",
-            targetMethod = "processPipeline",
-            action = "throw new java.io.IOException()")
-    public void testErrorInPipeline() throws Exception
-    {
-        System.out.println("testErrorInPipeline:");
-        StringBuffer buffer = new StringBuffer("hello world!");
-        buffer.append(" goodbye cruel world, goodbye!\n");
-        CharSequenceReader reader = new CharSequenceReader(buffer);
-        PatternReplacer replacer = new PatternReplacer("world", "mum",reader);
-        CharSequenceWriter writer = new CharSequenceWriter(replacer);
-        reader.start();
-        replacer.start();
-        writer.start();
-        reader.join();
-        replacer.join();
-        writer.join();
-        String output = writer.toString();
-        assert(output.equals(""));
-    }
-
-    /**
-     * this is a similar test to the previous one but it differs in two small respects. Firstly  the
-     * reader passes in many lines of text, enough to fill the input pipeline to the pattern replacer.
-     * Secondly, there are two rules, the first of which creates a countDown used to control firing
-     * of the second. The first rule is injected into the constructor for TextLineProcessor. It
-     * creates a countDown with count 2 using the newly constructed instance as an identifying key.<p/>
-     *
-     * The second rule throws an IOException in method processPipeline as in the previous test. However
-     * this time it specifies a target location and condition (note that these values were defaulted
-     * to "AT ENTRY" and "TRUE" in the previous example). The target location is inside the loop body,
-     * just before a call is made to method transform(String). This means the rule is triggered
-     * each time a line of text is about to be processed and written to the output.<p/>
-     *
-     * The condition calls countDown passing $0 as the identifying key. This ensures that the countdown
-     * used to perform the test is the one with initial counter 2 created when the text processor was
-     * constructed and that this same countDown is used on each successive firing. At the first
-     * triggering method countDown decrements the counter from 2 to 1 and returns false. At the second
-     * triggering it decrements it from 1 to 0 and again returns false. At the third firing it finds
-     * that the counter is zero so it deletes the countDown instance returning true. So, at this
-     * triggering the rule fires and throws an exception.<p/>
-     *
-     * The pattern replacer should process two lines of text before the exception is thrown. It should
-     * catch and print the exception, close its input and output streams and exit, causing the writer to
-     * finish with two lines of transformed output. The upstream thread should also print an exception.
-     * It will either still be writing text to its output or, more likely, it will be sitting on a full
-     * pipeline waiting for the pattern replacer to clear the pipe. In either case a close on the stream
-     * it is feeding will cause it to suffer a Pipe closed IOException.
-     * @throws Exception
-     */
-    @Test
-    @BMRules(rules={@BMRule(name="create countDown for TextLineProcessor",
-                    targetClass = "TextLineProcessor",
-                    targetMethod = "<init>",
-                    action = "createCountDown($0, 2)"),
-                    @BMRule(name="throw IOException at 3rd transform",
-                    targetClass = "TextLineProcessor",
-                    targetMethod = "processPipeline",
-                    targetLocation = "CALL transform(String)",
-                    condition = "countDown($0)",
-                    action = "throw new java.io.IOException()")})
-    public void testErrorInStuffedPipeline() throws Exception
-    {
-        System.out.println("testErrorInStuffedPipeline:");
-        StringBuffer buffer = new StringBuffer("hello world!\n");
-        buffer.append("goodbye cruel world, goodbye!\n");
-        for (int i = 0; i < 40; i++) {
-            buffer.append("goodbye! goodbye! goodbye!\n");
-        }
-        CharSequenceReader reader = new CharSequenceReader(buffer);
-        PatternReplacer replacer = new PatternReplacer("world", "mum",reader);
-        CharSequenceWriter writer = new CharSequenceWriter(replacer);
-        reader.start();
-        replacer.start();
-        writer.start();
-        reader.join();
-        replacer.join();
-        writer.join();
-        String output = writer.toString();
-        assert(output.equals("hello mum!\ngoodbye cruel mum, goodbye!\n"));
-    }
 }
